a1dc5b762468fad659ff6e9f48a7ac0def5a9eb2
majorana9ja <102370182+majorana9ja@users.noreply.github.com>
2022-03-31 19:25:14 +0545

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 def get_new_tab():
-    proxy_bot = "screenBot2022Username-rotate:screenBot2022Pwd@p.webshare.io:80/"
+    proxy_bot = "screenBot2022Username-rotate:screenBot2022Pwd@p.webshare.io:80"
     opts = Options()
     opts.add_argument('--disable-blink-features=AutomationControlled')
     opts.binary_location = os.environ.get("GOOGLE_CHROME_BIN")    
@@ -36,7 +36,7 @@ def get_new_tab():
     opts.add_argument("--start-maximized")
     opts.add_argument("--headless")
     opts.add_argument("--window-size=1920,1080")
-    opts.add_argument('--proxy-server=http://%s' % proxy_bot)   
+    opts.add_argument('--proxy-server=http://screenBot2022Username-rotate:screenBot2022Pwd@p.webshare.io:80')   
     
 
     service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
